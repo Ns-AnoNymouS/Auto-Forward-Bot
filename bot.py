d028c88d189d6d486c6cac51c96db359b6033c35
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-30 19:46:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,28 @@
+import pyrogram
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+
+from config import Config
+from pyrogram import Client 
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = Client(
+        "auto",
+        bot_token=Config.TG_BOT_TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins,
+        workers=100
+    )
+
+    app.run()
 
