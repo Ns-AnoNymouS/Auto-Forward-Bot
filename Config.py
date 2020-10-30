8720db73fdbea2606544be1aded5ae2a3c29aece
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-30 19:53:23 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,3 +4,4 @@ class Config(object):
       TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
       APP_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
+      CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
