df860ac38ae8d1f674a209ae3918113e3650240d
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-30 20:13:55 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -3,5 +3,6 @@ from pyrogram import Filters
 from bot.py import channelforward
 
 
-@channelforward.on_message()
+@channelforward.on_message(Filter.private)
 async def forward(c, m):
+    print("working")
