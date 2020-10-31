4b832e04940da46942ab5d5e0877af7431bdc3f6
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-31 14:55:21 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -1,2 +1,12 @@
 import pyrogram
 from pyrogram import filters
+from bot import channelforward
+from config import Config
+from translation import Translation
+
+@channelforward.on_message(filters.command(["start"]))
+async def start(c, m)
+      await m.send_message(chat_id=m.chat.id,
+                           text=Translation.start,
+                           parse_mode="html",
+                           quote=True)
