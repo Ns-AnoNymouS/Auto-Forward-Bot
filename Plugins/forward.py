0dc9ab984849e16622f3c0445382c7d50f80519d
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-31 14:18:49 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -1,7 +1,7 @@
 import pyrogram
 from pyrogram import filters
 from bot import channelforward
-from Config import Config 
+from config import Config 
 
 @channelforward.on_message(filters.private)
 async def forward(c, m):
