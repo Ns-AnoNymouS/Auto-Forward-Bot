69bc2114ef8fa1951d6a4946642087761be52b5b
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-31 15:02:42 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -10,7 +10,7 @@ from translation import Translation
 
 @channelforward.on_message(filters.command(["start"]))
 async def start(c, m):
-      await m.send_message(chat_id=m.chat.id,
+      await c.send_message(chat_id=m.chat.id,
                            text=Translation.start,
                            parse_mode="html",
                            quote=True)
