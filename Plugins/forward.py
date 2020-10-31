3a528e69213a6a5bdb1d66155620d4a87f499828
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-31 14:42:56 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -3,10 +3,12 @@ from pyrogram import filters
 from bot import channelforward
 from config import Config 
 
-@channelforward.on_message(filters.private)
+@channelforward.on_message(filters.channel)
 async def forward(c, m):
     for id in Config.CHANNEL:
        from_channel, to_channel = id.split(":")
+       if m.chat.id == from_channel:
+          await m.forward(to_channel)
        print(from_channel)
        print(to_channel)
     print(Config.CHANNEL)
