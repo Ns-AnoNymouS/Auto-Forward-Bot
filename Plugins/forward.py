e4dc592ade00b514eb0424ec0b86ab39a6f11fb6
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-31 14:37:34 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -6,7 +6,7 @@ from config import Config
 @channelforward.on_message(filters.private)
 async def forward(c, m):
     for id in Config.CHANNEL:
-       from, to = await Config.CHANNEL.split(":")
+       from, to = await Config.CHANNEL[str(id)].split(":")
        print(from)
        print(to)
     print(Config.CHANNEL)
