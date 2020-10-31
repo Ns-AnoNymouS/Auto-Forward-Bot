63526cd2b16122979c7e724c09ee89b0f422ef03
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-31 15:22:43 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,9 +1,9 @@
 class Translation(object):
       START = "Hi I am a channel auto forward bot clone of <a href='https://t.me/Autoforward_Nsbot'>Channel Automatic NS bot</a>"             
       ABOUT = """
-📝 Language: Python 3
-🧰 Framework: Pyrogram
-👨‍💻 Developer: 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
-📢 Channel: NS BOT UPDATES
-👥 Group: Ns BOT SUPPORT
-🌐Source Code: Press Me 🥰"""
+<b>📝 Language:</b> Python 3
+<b>🧰 Framework:</b> Pyrogram
+<b>👨‍💻 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
+<b>📢 Channel:</b> NS BOT UPDATES
+<b>👥 Group:</b> Ns BOT SUPPORT
+<b>🌐Source Code:</b> Press Me 🥰"""
