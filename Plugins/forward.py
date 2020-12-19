04ae75796e24b2e6be69e3c428383eb29aac38eb
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-12-19 20:35:24 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -10,8 +10,9 @@ from config import Config
 @channelforward.on_message(filters.channel)
 async def forward(c, m):
     # Forwarding the messages to the channel
-    print(Config.CHANNEL)
+
     for id in Config.CHANNEL:
        from_channel, to_channel = id.split(":")
        if m.chat.id == int(from_channel):
           await m.forward(int(to_channel), as_copy=True)
+          print("Forwarded a message from", from_channel, "to", to_channel)
