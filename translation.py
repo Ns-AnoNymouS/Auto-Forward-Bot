64492d4909d07420044e5a4298db5141db9be740
Disneyteam <85325455+Disneyteam76@users.noreply.github.com>
2021-07-25 10:57:59 +0530

    Update

--- translation.py
+++ translation.py
@@ -1,13 +1,13 @@
 class Translation(object):
 
       # Start text
-      START = "Hi I am a channel auto forward bot clone of <a href='https://t.me/Autoforward_Nsbot'>Channel Automatic NS bot</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @Ns_bot_updates, @Ns_Bot_supporters"             
+      START = "Hi I am a Channel Automatic Sender BOT /n By @doreamonfans1<b>Support Channel and Groups"             
 
       #About text
       ABOUT = """
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
-<b>👨‍💻 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
-<b>📢 Channel:</b> NS BOT UPDATES
-<b>👥 Group:</b> Ns BOT SUPPORT
-<b>🌐Source Code:</b> Press Me 🥰"""
+<b>👨‍💻 Developer:</b> [Doraemonfans](https://t.me/doreamonfans2)
+<b>📢 Channel:</b> [Disney Team](https://t.me/disneygrou)
+<b>👥 Group:</b> [Disney Team Chat](https://t.me/disneyteamchat)
+<b> 😁Want to make a own bot ask:</b> [my owner](https://t.me/doreamonfans2)
