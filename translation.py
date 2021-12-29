93f714043240d3e492c9d09d3ba14837ca346f8b
mukundambbs <83640801+mukundambbs@users.noreply.github.com>
2021-12-29 09:13:11 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,11 @@
 class Translation(object):
 
       # Start text
-      START = "Hi I am a channel auto forward bot clone of <a href='https://t.me/Autoforward_Nsbot'>Channel Automatic NS bot</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @Ns_bot_updates, @Ns_Bot_supporters"             
+      START = "Hi I am a channel auto forward bot of @LinkZz_MBBS            
 
       #About text
       ABOUT = """
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
-<b>👨‍💻 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
-<b>📢 Channel:</b> NS BOT UPDATES
-<b>👥 Group:</b> Ns BOT SUPPORT
-<b>🌐Source Code:</b> Press Me 🥰"""
+<b>👨‍💻 Developer:</b> ≡ 𝗠𝗨𝗞𝗨𝗡𝗗𝗔 ≡
+<b>👥 Group:</b> @LinkZz_MBBS
